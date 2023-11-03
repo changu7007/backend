2511783e5889a00589cc599ab11442429eac454c
Gagan CG <64187206+changu7007@users.noreply.github.com>
2023-11-03 12:01:17 +0530

    phonepe final fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -595,7 +595,7 @@ export const paymentVerification = async (req, res) => {
         .digest("hex") + `###${saltIndex}`;
 
     const response = await axios.get(
-      `${process.env.PHONE_PE_PROD_API_URL}/status/${input.merchantId}/${input.transactionId}`,
+      `${process.env.PHONE_PE_PROD_API_STATUS_URL}/${input.merchantId}/${input.transactionId}`,
       {
         headers: {
           "Content-Type": "application/json",
